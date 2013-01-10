a6cb291f87a196d78d5835419ab44a2dd2a58a58
Jason Ward <jason.ward@policystat.com>
2013-01-10 15:39:10 -0500

    added a setup file

---
+++ setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages  # noqa
+
+long_description = codecs.open("README.rst", "r", "utf-8").read()
+
+setup(
+    name="docx2html",
+    version="0.0.1",
+    description="docx (OOXML) to html converter",
+    author="Jason Ward",
+    author_email="jason.louard.ward@gmail.com",
+    url="http://github.com/PolicyStat/docx2html/",
+    platforms=["any"],
+    license="BSD",
+    packages=find_packages(),
+    scripts=[],
+    zip_safe=False,
+    install_requires=['lxml', 'PIL'],
+    cmdclass={},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Text Processing :: Markup :: XML",
+    ],
+    long_description=long_description,
+)
